bc034e32b3ef84b355a48ce298a689415357ad66
Mikael <mikaelmuniz2001@gmail.com>
2023-09-11 10:59:17 -0300

    fix: header responsivity

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -3,9 +3,9 @@ import React from "react"
 export default function Header() {
     return <>
         <div className="bg-banner w-screen h-full font-spacemono">
-            <div className="p-32 grid gap-4">
-                <span className="text-6xl">WEB SCRAPER DE<br />FISCALIZAÇÃO</span>
-                <span className="text-3xl">Utilizando automação para fiscalizar o governo</span>
+            <div className="sm:p-32 p-12 grid gap-4">
+                <span className="sm:text-6xl text-3xl">WEB SCRAPER DE<br />FISCALIZAÇÃO</span>
+                <span className="sm:text-3xl text-xl">Utilizando automação para fiscalizar o governo</span>
             </div>
         </div>
     </>
